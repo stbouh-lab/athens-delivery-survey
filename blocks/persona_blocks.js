fdcc4b6fad72ae7f9de545e0343bcca14bb601a7
stbouh-lab <stbouh@gmail.com>
2025-11-26 15:11:23 +0200

    persona_blocks.js

--- blocks/persona_blocks.js
+++ blocks/persona_blocks.js
@@ -9,15 +9,15 @@ const PERSONA_BLOCKS = [
 {BlockID:1, Set:2, Alt:2, Cost:0.5,Walk:100,CO2save:40, Reliability:90, Resched:2, Noise:30, ModeFlag:1},
 {BlockID:1, Set:2, Alt:3, Cost:1, Walk:250,CO2save:55, Reliability:95, Resched:0, Noise:50, ModeFlag:2},
 {BlockID:1, Set:3, Alt:1, Cost:1, Walk:0, CO2save:0,  Reliability:75, Resched:2, Noise:0,  ModeFlag:0},
-{BlockID:1, Set:3, Alt:2, Cost:0, Walk:200,CO2save:40, Reliability:90, Resched:2, Noise:30, ModeFlag=1},
+{BlockID:1, Set:3, Alt:2, Cost:0, Walk:200,CO2save:40, Reliability:90, Resched:2, Noise:30, ModeFlag:1},
 {BlockID:1, Set:3, Alt:3, Cost:0.5,Walk:150,CO2save:55, Reliability:95, Resched:0, Noise:50, ModeFlag:2},
 {BlockID:1, Set:4, Alt:1, Cost:0, Walk:0, CO2save:0,  Reliability:75, Resched:2, Noise:0,  ModeFlag:0},
 {BlockID:1, Set:4, Alt:2, Cost:1, Walk:100,CO2save:40, Reliability:90, Resched:2, Noise:30, ModeFlag:1},
 {BlockID:1, Set:4, Alt:3, Cost:0, Walk:250,CO2save:55, Reliability:95, Resched:0, Noise:50, ModeFlag:2},
-{BlockID:1, Set:5, Alt:1, Cost:0.5,Walk:0, CO2save:0,  Reliability:75, Resched=2, Noise:0,  ModeFlag=0},
+{BlockID:1, Set:5, Alt:1, Cost:0.5,Walk:0, CO2save:0,  Reliability:75, Resched:2, Noise:0,  ModeFlag:0},
 {BlockID:1, Set:5, Alt:2, Cost:0, Walk:150,CO2save:40, Reliability:90, Resched:2, Noise:30, ModeFlag:1},
 {BlockID:1, Set:5, Alt:3, Cost:1, Walk:200,CO2save:55, Reliability:95, Resched:0, Noise:50, ModeFlag:2},
-{BlockID:1, Set:6, Alt:1, Cost:0, Walk:0, CO2save:0,  Reliability:75, Resched:2, Noise:0,  ModeFlag=0},
+{BlockID:1, Set:6, Alt:1, Cost:0, Walk:0, CO2save:0,  Reliability:75, Resched:2, Noise:0,  ModeFlag:0},
 {BlockID:1, Set:6, Alt:2, Cost:0.5,Walk:100,CO2save:40, Reliability:90, Resched:2, Noise:30, ModeFlag:1},
 {BlockID:1, Set:6, Alt:3, Cost:1, Walk:150,CO2save:55, Reliability:95, Resched:0, Noise:50, ModeFlag:2},
 
@@ -121,4 +121,5 @@ const PERSONA_BLOCKS = [
 {BlockID:6, Set:6, Alt:2, Cost:0.5,Walk:100,CO2save:40, Reliability:90, Resched:2, Noise:30, ModeFlag:1},
 {BlockID:6, Set:6, Alt:3, Cost:1, Walk:150,CO2save:55, Reliability:95, Resched:0, Noise:50, ModeFlag:2}
 ];
+
 
